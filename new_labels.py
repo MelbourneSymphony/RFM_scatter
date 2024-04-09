2a166dfeafdd5db1e5c4eae28932ffdb1ab30dba
Sam Harvey <70475300+Melsuser5@users.noreply.github.com>
2024-04-09 11:37:34 +1000

    Update new_labels.py
    updated for Quarter 1 2024

--- new_labels.py
+++ new_labels.py
@@ -4,10 +4,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://raw.githubusercontent.com/Melsuser5/RFM_labels/main/23_q4_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/Melsuser5/RFM_labels/main/rfm_2024_q1_predictions.csv')
 custom_palette = ["#5a8eb8", "#5ab874", "#bf3636", "#f08922", "#8146d4", "#e3528e", "#2a9ac7"]
-fig_all = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment',width=1600, height=800,color_continuous_scale=custom_palette)
-log_fig = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment', log_x=False,log_y=True,log_z=True, width=1600, height=800,color_continuous_scale=custom_palette)
+fig_all = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='2024_q1_segment',width=1600, height=800,color_continuous_scale=custom_palette)
+log_fig = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='2024_q1_segment', log_x=False,log_y=True,log_z=True, width=1600, height=800,color_continuous_scale=custom_palette)
 
 st.set_page_config(layout="centered")
 st.set_option('deprecation.showPyplotGlobalUse', True)
@@ -24,13 +24,13 @@ st.write("Segmentation Flows Click [here](https://rfmflow-o3zzye2ivmsgdfx4akmdoy
 st.header("Segment Descriptions and Database Count")
 
 segment_data = [
-    {"Segment": "0 – Slipping", "Description": "Customers who have not purchased within the last year", "Customer Count": 31020},
-    {"Segment": "1 - Lost Touch", "Description": "Customers who have not purchased since 2020", "Customer Count": 33645},
-    {"Segment": "2 - New Customers", "Description": "Customers that have made at least one purchase in the last year", "Customer Count": 46943},
-    {"Segment": "3 - Faithful", "Description": "Customers who return often, but do not spend as much in each transaction compared to other segments (average of $118)", "Customer Count": 3077},
-    {"Segment": "4 - Loyal Purchasers", "Description": "Purchase most often compared to other segments (top 5% frequency)", "Customer Count": 436},
-    {"Segment": "5 - Affluent", "Description": "Customers who spend over $1,200 per transaction", "Customer Count": 1176},
-    {"Segment": "6 - 'Top Tier’", "Description": "Top 5% total ticket spend", "Customer Count": 407}
+    {"Segment": "0 – Slipping", "Description": "Customers who have not purchased within the last year", "Customer Count": 38527},
+    {"Segment": "1 - Lost Touch", "Description": "Customers who have not purchased since 2020", "Customer Count": 22968},
+    {"Segment": "2 - New Customers", "Description": "Customers that have made at least one purchase in the last year", "Customer Count": 42033},
+    {"Segment": "3 - Faithful", "Description": "Customers who return often, but do not spend as much in each transaction compared to other segments (average of $118)", "Customer Count": 1030},
+    {"Segment": "4 - Loyal Purchasers", "Description": "Purchase most often compared to other segments (top 5% frequency)", "Customer Count": 95},
+    {"Segment": "5 - Affluent", "Description": "Customers who spend over $1,200 per transaction", "Customer Count": 2813},
+    {"Segment": "6 - 'Top Tier’", "Description": "Top 5% total ticket spend", "Customer Count": 2857}
 ]
 seg_count = pd.DataFrame(segment_data)
 seg_count.set_index(['Segment'],inplace=True)
