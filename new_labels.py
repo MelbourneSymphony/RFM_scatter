b2991998e4e62dbe66aa1ae959cd0fb5ea2de2d2
MelbourneSymphony <harveys@mso.com.au>
2024-11-06 16:14:27 +1100

    added nov 2024 data

--- new_labels.py
+++ new_labels.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://raw.githubusercontent.com/Melsuser5/RFM_labels/main/rfm_july_2024_rfm_july_2024_predictions_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/MelbourneSymphony/RFM_scatter/refs/heads/main/predictions_rfm_nov_2024_predictions.csv')
 custom_palette = ["#5a8eb8", "#5ab874", "#bf3636", "#f08922", "#8146d4", "#e3528e", "#2a9ac7"]
 fig_all = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment',width=1600, height=800,color_continuous_scale=custom_palette)
 log_fig = px.scatter_3d(df, x='recency', y='frequency', z='revenue', color='Segment', log_x=False,log_y=True,log_z=True, width=1600, height=800,color_continuous_scale=custom_palette)
@@ -25,13 +25,13 @@ st.write("Segmentation Flows Click [here](https://mso-rfmflow.streamlit.app/)")
 st.header("Segment Descriptions and Database Count")
 
 segment_data = [
-    {"Segment": "0 – Slipping", "Description": "Customers who have not purchased within the last year", "Customer Count": 44500},
-    {"Segment": "1 - Lost Touch", "Description": "Customers who have not purchased since 2020", "Customer Count": 20100},
-    {"Segment": "2 - New Customers", "Description": "Customers that have made at least one purchase in the last year", "Customer Count": 45900},
-    {"Segment": "3 - Faithful", "Description": "Customers who return often, but do not spend as much in each transaction compared to other segments", "Customer Count": 1090},
+    {"Segment": "0 – Slipping", "Description": "Customers who have not purchased within the last year", "Customer Count": 55640},
+    {"Segment": "1 - Lost Touch", "Description": "Customers who have not purchased since 2020", "Customer Count": 8656},
+    {"Segment": "2 - New Customers", "Description": "Customers that have made at least one purchase in the last year", "Customer Count": 42552},
+    {"Segment": "3 - Faithful", "Description": "Customers who return often, but do not spend as much in each transaction compared to other segments", "Customer Count": 459},
     {"Segment": "4 - Loyal Purchasers", "Description": "Purchase most often compared to other segments (top 5% frequency)", "Customer Count": 140},
-    {"Segment": "5 - Affluent", "Description": "Customers who Purchase in higher reserves", "Customer Count": 3030},
-    {"Segment": "6 - 'Top Tier’", "Description": "Top 5% total ticket spend", "Customer Count": 500}
+    {"Segment": "5 - Affluent", "Description": "Customers who Purchase in higher reserves", "Customer Count": 2511},
+    {"Segment": "6 - 'Top Tier’", "Description": "Top 5% total ticket spend", "Customer Count": 294}
 ]
 seg_count = pd.DataFrame(segment_data)
 seg_count.set_index(['Segment'],inplace=True)
